ad1217af0d1d32080db58b388c7813588c9dfa46
klnkr <klnkr@cock.li>
2015-10-13 14:30:49 +0200

    use BF and remove techies shit

--- AutoDeward/AutoDeward/Deward.cs
+++ AutoDeward/AutoDeward/Deward.cs
@@ -7,70 +7,69 @@ using Ensage;
 using SharpDX;
 using SharpDX.Direct3D9;
 
-namespace AutoDeward
-{
-    internal class Deward
-    {
+namespace AutoDeward {
+    internal class Deward {
         private static Item quellingBlade;
         private static Item tango;
-        private static Team enemyTeam = Team.Neutral;
 
         private static int sleepTime;
 
 
-        public static void Init()
-        {
+        public static void Init() {
             Game.OnUpdate += GameOnUpdate;
         }
 
-        private static void GameOnUpdate(EventArgs eventArgs)
-        {
+        private static void GameOnUpdate(EventArgs eventArgs) {
             var me = ObjectMgr.LocalHero;
 
             if (!Game.IsInGame || me == null) return;
-            if (sleepTime > 0)
-            {
+            if (sleepTime > 0) {
                 sleepTime--;
                 return;
             }
 
-            enemyTeam = me.Team == Team.Dire ? Team.Radiant : Team.Dire;
-
-            quellingBlade = me.Inventory.Items.FirstOrDefault(i => i.ClassID == ClassID.CDOTA_Item_QuellingBlade);
+            quellingBlade =
+                me.Inventory.Items.FirstOrDefault(
+                    i => i.ClassID == ClassID.CDOTA_Item_QuellingBlade || i.ClassID == ClassID.CDOTA_Item_Battlefury);
             tango =
                 me.Inventory.Items.FirstOrDefault(
                     i => i.ClassID == ClassID.CDOTA_Item_Tango || i.ClassID == ClassID.CDOTA_Item_Tango_Single);
 
-            var canDeward = (quellingBlade != null || tango != null);
+            var units = ObjectMgr.GetEntities<Unit>();
 
-            var wards = ObjectMgr.GetEntities<Unit>()
-                .Where(u => (u.ClassID == ClassID.CDOTA_NPC_Observer_Ward || u.ClassID == ClassID.CDOTA_NPC_Observer_Ward_TrueSight) && u.Team == enemyTeam && u.IsAlive && Vector3.Distance(me.Position, u.Position) < 475).ToList();
+            var wards = units
+                .Where(
+                    u => (u.ClassID == ClassID.CDOTA_NPC_Observer_Ward || u.ClassID == ClassID.CDOTA_NPC_Observer_Ward_TrueSight) && u.Team != me.Team && u.IsAlive && Vector3.Distance(me.Position, u.Position) < 475).ToList();
 
-            if (canDeward)
-            {
+            var mines = units.Where(u => u.ClassID == ClassID.CDOTA_NPC_TechiesMines && u.Team != me.Team && u.IsAlive && Vector3.Distance(me.NetworkPosition, u.NetworkPosition) < 475).ToList();
+
+            var canDewardWard = ((quellingBlade != null || tango != null) && wards.Count > 0);
+            var canDewardMine = ((quellingBlade != null) && mines.Count > 0);
+
+            if (canDewardWard) {
                 Item dewardItem = quellingBlade;
 
                 // is using a tango worth it?
                 // tango heals 230 if used on ward, 115 if used on tree
                 // TODO: check if already under tango effect
-                if (tango != null && quellingBlade != null)
-                {
+                if (tango != null && quellingBlade != null && me.Modifiers.All(m => m.Name != "modifier_tango_heal")) {
                     var hpMissing = me.MaximumHealth - me.Health;
-                    if (hpMissing > 120)
-                    {
-                        dewardItem = tango;
-                    }
+                    if (hpMissing > 115) dewardItem = tango;
                 }
-                else if (quellingBlade == null)
-                {
-                    dewardItem = tango;
-                }
+                else if (quellingBlade == null) dewardItem = tango;
 
 
-                if (wards.Count > 0 && dewardItem.Cooldown == 0)
-                {
+                if (dewardItem.Cooldown == 0) {
                     dewardItem.UseAbility(wards[0]);
-                    Console.WriteLine("Dewarded a ward");
+                    Console.WriteLine("Dewarded a ward with: " + dewardItem.Name);
+                    sleepTime = 10;
+                }
+            }
+
+            if (canDewardMine) {
+                if (quellingBlade.Cooldown == 0) {
+                    quellingBlade.UseAbility(mines[0]);
+                    Console.WriteLine("Demined a mine");
                     sleepTime = 10;
                 }
             }
