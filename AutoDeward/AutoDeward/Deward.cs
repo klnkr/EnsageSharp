e86efe655ab9ca3fd37931b540ecb872d7225f73
Klinker <Klinker@BENIS>
2016-07-10 20:12:45 +0200

    add menu and some options

--- AutoDeward/AutoDeward/Deward.cs
+++ AutoDeward/AutoDeward/Deward.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Ensage;
+using Ensage.Common.Menu;
+using Ensage.Common.Menu.MenuItems;
 using SharpDX;
 using SharpDX.Direct3D9;
 
@@ -14,18 +16,49 @@ namespace AutoDeward {
 
         private static int sleepTime;
 
+        private static Menu menu;
+
+        private static readonly string[] breakableInvisModifiers = {
+            "modifier_nyx_assassin_vendetta",
+            "modifier_invoker_ghost_walk",
+            "modifier_rune_invis",
+            "modifier_item_invisibility_edge_windwalk",
+            "modifier_item_silver_edge_windwalk",
+            "modifier_bounty_hunter_wind_walk",
+            "modifier_clinkz_wind_walk",
+            "modifier_weaver_shukuchi"
+        };
 
         public static void Init() {
+            menu = new Menu("AutoDeward", "autodeward", true);
+            menu.AddItem(new OnOffSlider("enabled", "Enabled", true));
+            menu.AddItem(new OnOffSlider("dontBreakInvisibility", "Don't break invisibility", true));
+            menu.AddToMainMenu();
             Game.OnUpdate += GameOnUpdate;
         }
 
         private static void GameOnUpdate(EventArgs eventArgs) {
+            if (!menu.Item("enabled").GetValue<bool>()) return;
+
             var me = ObjectManager.LocalHero;
 
             if (!Game.IsInGame || me == null) return;
             if (sleepTime > 0) {
                 sleepTime--;
                 return;
+            }
+
+            sleepTime = 10;
+
+            if (menu.Item("dontBreakInvisibility").GetValue<bool>()) {
+                var modifiers = me.Modifiers;
+                foreach (var modifier in modifiers) {
+                    foreach (var breakableInvisModifier in breakableInvisModifiers) {
+                        if (modifier.Name == breakableInvisModifier) {
+                            return;
+                        }
+                    }
+                }
             }
 
             quellingBlade =
@@ -69,8 +102,6 @@ namespace AutoDeward {
                     quellingBlade.UseAbility(mines[0]);
                 }
             }
-
-            sleepTime = 10;
         }
     }
 }
